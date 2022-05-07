05f62bd39cb6aba58ad7046c3d99dfdf8e6f4506
Ashwin Venkita Subharaman (s3783614) <s3783614@student.rmit.edu.au>
2022-05-08 03:05:15 +1000

    Preparing to start switching between different activities.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button basicMode;
+    Button scientificMode;
+    Button programmerMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        basicMode = findViewById(R.id.btnBasicMode);
+        scientificMode = findViewById(R.id.btnScientificMode);
+        programmerMode = findViewById(R.id.btnProgrammerMode);
+
+
+
     }
 }
