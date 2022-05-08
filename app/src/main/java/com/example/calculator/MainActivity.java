3733e891f9371e1abf7862ec9fe28befe96f6a3c
Ashwin Venkita Subharaman (s3783614) <s3783614@student.rmit.edu.au>
2022-05-08 10:30:22 +1000

    Activities for all the different modes created and switching between activities implemented

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -16,11 +19,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        basicMode = findViewById(R.id.btnBasicMode);
-        scientificMode = findViewById(R.id.btnScientificMode);
-        programmerMode = findViewById(R.id.btnProgrammerMode);
-
+//        basicMode = findViewById(R.id.btnBasicMode);
+//        scientificMode = findViewById(R.id.btnScientificMode);
+//        programmerMode = findViewById(R.id.btnProgrammerMode);
 
 
     }
+
+    public void onCLick(View v)
+    {
+        Intent intent;
+        switch(v.getId())
+        {
+            case R.id.btnBasicMode:
+                intent = new Intent(MainActivity.this, BasicCalculator.class);
+                startActivity(intent);
+
+                break;
+
+            case R.id.btnProgrammerMode:
+                intent = new Intent(MainActivity.this, ProgrammerCalculator.class);
+                startActivity(intent);
+
+                break;
+
+            case R.id.btnScientificMode:
+                intent = new Intent(MainActivity.this, ScientificCalculator.class);
+                startActivity(intent);
+
+                break;
+        }
+    }
+
 }
