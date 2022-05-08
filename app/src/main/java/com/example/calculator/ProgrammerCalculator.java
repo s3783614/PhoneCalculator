199dfa117f1dd0367a6c23d8fbc35e8aa60ffccb
Ashwin Venkita Subharaman (s3783614) <s3783614@student.rmit.edu.au>
2022-05-08 10:42:22 +1000

    Implemented the home button in different activities

--- app/src/main/java/com/example/calculator/ProgrammerCalculator.java
+++ app/src/main/java/com/example/calculator/ProgrammerCalculator.java
@@ -3,12 +3,27 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProgrammerCalculator extends AppCompatActivity {
+
+    Button btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programmer_calculator);
+
+        btnHome = findViewById(R.id.btnProHome);
+        btnHome.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/calculator/ScientificCalculator.java
+++ app/src/main/java/com/example/calculator/ScientificCalculator.java
@@ -3,12 +3,25 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScientificCalculator extends AppCompatActivity {
 
+    Button btnHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scientific_calculator);
+
+        btnHome = findViewById(R.id.btnScientificHome);
+        btnHome.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
     }
 }
