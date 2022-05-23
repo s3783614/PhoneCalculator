41f41ea0a847a979abc00d6d9ddd024012e95195
Ashwin Venkita Subharaman (s3783614) <s3783614@student.rmit.edu.au>
2022-05-23 23:24:34 +1000

    working on validation and equals button implemented on basic calculator

--- app/src/main/java/com/example/calculator/BasicCalculator.java
+++ app/src/main/java/com/example/calculator/BasicCalculator.java
@@ -3,13 +3,18 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import org.mariuszgromada.math.mxparser.*;
 
 public class BasicCalculator extends AppCompatActivity {
 
 //    Button btnHome;
+    private boolean operationChecker = false;
+
+
     private TextView previousCalc;
     private EditText userInput;
 
@@ -20,7 +25,7 @@ public class BasicCalculator extends AppCompatActivity {
 
         previousCalc = findViewById(R.id.lblPreviousCalculation);
         userInput = findViewById(R.id.txtDisplayText);
-
+        userInput.setShowSoftInputOnFocus(false);
 
 //        btnHome = findViewById(R.id.btnBasicHome);
 //        btnHome.setOnClickListener(new View.OnClickListener()
@@ -43,91 +48,128 @@ public class BasicCalculator extends AppCompatActivity {
         String leftString = oldString.substring(0,cursorPosition);
         String rightString = oldString.substring(cursorPosition);
 //        "ASH | WIN"
-        userInput.setText(String.format("%s%s%s", leftString, inputToAdd, rightString));
-        userInput.setSelection(cursorPosition+inputToAdd.length());
+//        if(!operationChecker)
+//        {
+            userInput.setText(String.format("%s%s%s", leftString, inputToAdd, rightString));
+            userInput.setSelection(cursorPosition+inputToAdd.length());
+//        }
     }
 
     public void zeroBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnZero));
     }
 
     public void oneBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnOne));
     }
 
     public void twoBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnTwo));
     }
     public void threeBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnThree));
     }
     public void fourBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnFour));
     }
     public void fiveBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnFive));
     }
     public void sixBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnSix));
     }
     public void sevenBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnSeven));
     }
     public void eightBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnEight));
     }
     public void nineBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnNine));
     }
     public void parenthesesOpenBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnParenthesesOpen));
     }
     public void parenthesesCloseBTN(View view)
     {
+//        operationChecker = false;
         updateUserInput(getResources().getString(R.string.btnParenthesesClose));
     }
     public void divideBTN(View view)
     {
-        updateUserInput(getResources().getString(R.string.btnDecimal));
+        updateUserInput(getResources().getString(R.string.btnDivide));
+//        operationChecker = true;
     }
     public void multiplyBTN(View view)
     {
         updateUserInput(getResources().getString(R.string.btnMultiply));
+//        operationChecker = true;
     }
     public void subtractBTN(View view)
     {
         updateUserInput(getResources().getString(R.string.btnSubtract));
+//        operationChecker = true;
     }
     public void addBTN(View view)
     {
         updateUserInput(getResources().getString(R.string.btnAdd));
+//        operationChecker = true;
     }
     public void decimalBTN(View view)
     {
         updateUserInput(getResources().getString(R.string.btnDecimal));
+//        operationChecker = true;
     }
 
     public void equalsBTN(View view)
     {
-//        updateUserInput();
+        String userExpression = userInput.getText().toString();
+
+        Expression exp = new Expression(userExpression);
+        //exp.calculate -> in the MX parser library
+        String solution = String.valueOf((exp.calculate()));
+
+        userInput.setText(solution);
+        userInput.setSelection(userInput.getText().length());
     }
     public void clearBTN(View view)
     {
-        updateUserInput("");
+//        operationChecker = false;
+        userInput.setText("");
     }
     public void backspaceBTN(View view)
     {
-//        updateUserInput();
+        int cursorPositon = userInput.getSelectionStart();
+        int textLength = userInput.getText().length();
+
+        if(cursorPositon != 0 && textLength != 0)
+        {
+            SpannableStringBuilder selection = (SpannableStringBuilder)userInput.getText();
+            selection.replace(cursorPositon-1,cursorPositon,"");
+            userInput.setText(selection);
+            userInput.setSelection(cursorPositon-1);
+        }
     }
 }
